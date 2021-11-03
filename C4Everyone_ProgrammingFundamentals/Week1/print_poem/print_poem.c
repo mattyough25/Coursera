6cfb57f63a4915e6d3de8cfec330bb11c98290b4
mattyough25 <mgy0003@mix.wvu.edu>
2021-11-03 12:53:27 -0400

    Week1

---
+++ C4Everyone_ProgrammingFundamentals/Week1/print_poem/print_poem.c
@@ -0,0 +1,80 @@
+/* Print a Poem
+Matthew Yough
+2 November 2021
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+printf("\nCasey at the Bat\n");
+printf("By Ernest Lawrence Thayer\n\n");
+
+printf("The outlook wasn't brilliant for the Mudville nine that day\n"
+"The score stood four to two, with but one inning more to play,\n"
+"And then when Cooney died at first, and Barrows did the same,\n"
+"A pall-like silence fell upon the patrons of the game.\n\n"
+
+"A straggling few got up to go in deep despair. The rest\n"
+"Clung to the hope which springs eternal in the human breast;\n"
+"They thought, 'If only Casey could but get a whack at that \n"
+"We'd put up even money now, with Casey at the bat.'\n\n"
+
+"But Flynn preceded Casey, as did also Jimmy Blake,\n"
+"And the former was a hoodoo, while the latter was a cake;\n"
+"So upon that stricken multitude grim melancholy sat,\n"
+"For there seemed but little chance of Casey getting to the bat.\n\n"
+
+"But Flynn let drive a single, to the wonderment of all,\n"
+"And Blake, the much despised, tore the cover off the ball;\n"
+"And when the dust had lifted, and men saw what had occurred,\n"
+"There was Jimmy safe at second and Flynn a-hugging third.\n\n"
+
+"Then from five thousand throats and more there rose a lusty yell;\n"
+"It rumbled through the valley, it rattled in the dell;\n"
+"It pounded on the mountain and recoiled upon the flat,\n"
+"For Casey, mighty Casey, was advancing to the bat.\n\n"
+
+"There was ease in Casey's manner as he stepped into his place;\n"
+"There was pride in Casey's bearing and a smile lit Casey's face.\n"
+"And when, responding to the cheers, he lightly doffed his hat,\n"
+"No stranger in the crowd could doubt 'twas Casey at the bat.\n\n"
+
+"Ten thousand eyes were on him as he rubbed his hands with dirt;\n"
+"Five thousand tongues applauded when he wiped them on his shirt;\n"
+"Then while the writhing pitcher ground the ball into his hip,\n"
+"Defiance flashed in Casey's eye, a sneer curled Casey's lip.\n\n"
+
+"And now the leather-covered sphere came hurtling through the air,\n"
+"And Casey stood a-watching it in haughty grandeur there.\n"
+"Close by the sturdy batsman the ball unheeded sped \n"
+"'That ain't my style,' said Casey. 'Strike one!' the umpire said.\n\n"
+
+"From the benches, black with people, there went up a muffled roar,\n"
+"Like the beating of the storm-waves on a stern and distant shore;\n"
+"'Kill him! Kill the umpire!' shouted someone on the stand;\n"
+"And it's likely they'd have killed him had not Casey raised his hand.\n\n"
+
+"With a smile of Christian charity great Casey's visage shone;\n"
+"He stilled the rising tumult; he bade the game go on;\n"
+"He signaled to the pitcher, and once more the dun sphere flew;\n"
+"But Casey still ignored it and the umpire said, 'Strike two!'\n\n"
+
+"'Fraud!' cried the maddened thousands, and echo answered 'Fraud!'\n"
+"But one scornful look from Casey and the audience was awed.\n"
+"They saw his face grow stern and cold, they saw his muscles strain,\n"
+"And they knew that Casey wouldn't let that ball go by again.\n\n"
+
+"The sneer is gone from Casey's lip, his teeth are clenched in hate,\n"
+"He pounds with cruel violence his bat upon the plate;\n"
+"And now the pitcher holds the ball, and now he lets it go,\n"
+"And now the air is shattered by the force of Casey's blow.\n\n"
+
+"Oh, somewhere in this favoured land the sun is shining bright,\n"
+"The band is playing somewhere, and somewhere hearts are light;\n"
+"And somewhere men are laughing, and somewhere children shout,\n"
+"But there is no joy in Mudville, mighty Casey has struck out.\n\n");
+
+return 0;
+
+}
