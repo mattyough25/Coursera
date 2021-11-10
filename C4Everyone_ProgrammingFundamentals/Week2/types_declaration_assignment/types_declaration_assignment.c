f4dda825a7d1ce612bce0f1b782132b2b7fd762d
mattyough25 <mgy0003@mix.wvu.edu>
2021-11-09 20:52:13 -0500

    Week2 update

---
+++ C4Everyone_ProgrammingFundamentals/Week2/types_declaration_assignment/types_declaration_assignment.c
@@ -0,0 +1,18 @@
+/*
+Fundamental Types Declaration and Assignment
+Matthew Yough
+Nov. 9 2021
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+int a = 5, b = 7, c = 6; //declare and initialize
+double average = 0.0; //good practice
+
+printf("a = %d, b = %d, c = %d\n", a, b, c);
+average = (a + b + c) / 3.0; // conversion if 3
+printf("average = %lf\n", average);
+return 0;
+}
