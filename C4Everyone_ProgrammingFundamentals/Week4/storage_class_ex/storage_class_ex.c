3f413321a31c8f6fecc64f95e613a1fcd80ebdb6
mattyough25 <mgy0003@mix.wvu.edu>
2021-11-16 21:17:37 -0500

    Week4 Update

---
+++ C4Everyone_ProgrammingFundamentals/Week4/storage_class_ex/storage_class_ex.c
@@ -0,0 +1,30 @@
+/*
+Matthew Yough
+Storage Class Example
+November 16, 2021
+*/
+
+#include <stdio.h>
+
+extern int reps = 0;
+
+void f(void)
+{
+	static int called = 0;
+	printf("f called %d\n",called);
+	called++;
+	reps = reps + called;
+}
+
+int main(void)
+{
+	auto int i = 1;
+	const int Limit = 10;
+	for( i = 1; i < Limit; i++)
+	{
+		printf("i local = %d, reps global =%d\n",
+				i, reps);
+		f();
+	}
+	return 0;
+}
