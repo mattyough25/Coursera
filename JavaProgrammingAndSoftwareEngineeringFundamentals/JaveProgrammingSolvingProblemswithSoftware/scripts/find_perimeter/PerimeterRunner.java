b0cd2d41f0f1a289e13b33f2a680eeffab33bc4e
mattyough25 <mgy0003@mix.wvu.edu>
2022-05-12 21:00:33 -0400

    update

--- JavaProgrammingAndSoftwareEngineeringFundamentals/JaveProgrammingSolvingProblemswithSoftware/scripts/find_perimeter/PerimeterRunner.java
+++ JavaProgrammingAndSoftwareEngineeringFundamentals/JaveProgrammingSolvingProblemswithSoftware/scripts/find_perimeter/PerimeterRunner.java
@@ -22,13 +22,54 @@ public class PerimeterRunner {
     }
     
     public double getNumPoints(Shape s){
+        double points = 0;
         
+        for(Point currPt: s.getPoints()){
+            points = points + 1;
+        }
         return points;
     }
     
     public double getAverageLength(Shape s){
+        double tot_len = 0;
+        double num_sides = 0;
+        Point prevPt = s.getLastPoint();
         
+        for(Point currPt: s.getPoints()){
+            double currDist = prevPt.distance(currPt);
+            tot_len = tot_len + currDist;
+            num_sides = num_sides + 1;
+            prevPt = currPt;
+        }
+        
+        double avg_len = tot_len/num_sides;
         return avg_len;
+    }
+    
+    public double getLargestSide(Shape s){
+        double large_side = 0;
+        Point prevPt = s.getLastPoint();
+        
+        for(Point currPt: s.getPoints()){
+            double currDist = prevPt.distance(currPt);
+            if(currDist > large_side){
+                large_side = currDist;
+            }
+            prevPt = currPt;
+        }
+        return large_side;
+    }
+    
+    public double getLargestX(Shape s){
+        double largeX = 0;
+        double currX  = 0;
+        
+        for(Point currPt: s.getPoints()){
+            if(currPt.getX() > largeX){
+                largeX = currPt.getX();
+            }
+        }
+        return largeX;
     }
     
     public void testPerimeter(){
@@ -43,6 +84,12 @@ public class PerimeterRunner {
         
         double avg_len = getAverageLength(s);
         System.out.println("Average Length= " + avg_len);
+        
+        double large_side = getLargestSide(s);
+        System.out.println("Largest Side= " + large_side);
+        
+        double largeX = getLargestX(s);
+        System.out.println("Largest X= " + largeX);
     }
     
     public static void main(String[] args){
