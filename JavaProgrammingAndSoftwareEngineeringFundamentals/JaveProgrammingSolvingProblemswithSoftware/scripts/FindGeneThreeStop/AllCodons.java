5d49ad54484386b7fe1aaa9a13283655a9e97617
mattyough25 <mgy0003@mix.wvu.edu>
2022-05-23 19:09:02 -0400

    Update java

--- JavaProgrammingAndSoftwareEngineeringFundamentals/JaveProgrammingSolvingProblemswithSoftware/scripts/FindGeneThreeStop/AllCodons.java
+++ JavaProgrammingAndSoftwareEngineeringFundamentals/JaveProgrammingSolvingProblemswithSoftware/scripts/FindGeneThreeStop/AllCodons.java
@@ -5,6 +5,8 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+import edu.duke.*;
 public class AllCodons {
     public int findStopCodon(String dnaStr, int startIndex, String stopCodon){
         int currIndex = dnaStr.indexOf(stopCodon, startIndex+3);
@@ -21,8 +23,8 @@ public class AllCodons {
         return -1;
     }
     
-    public String findGene(String dna){
-        int startIndex = dna.indexOf("ATG");
+    public String findGene(String dna, int where){
+        int startIndex = dna.indexOf("ATG", where);
         if (startIndex == -1){
             return "";
         }
@@ -30,8 +32,6 @@ public class AllCodons {
         int taaIndex = findStopCodon(dna,startIndex,"TAA");
         int tagIndex = findStopCodon(dna,startIndex,"TAG");
         int tgaIndex = findStopCodon(dna,startIndex,"TGA");
-        //int temp = Math.min(taaIndex,tagIndex);
-        //int minIndex = Math.min(temp,tgaIndex);
         int minIndex = 0;
         if (taaIndex == -1 || (tgaIndex != -1 && tgaIndex < taaIndex)){
             minIndex = tgaIndex;
@@ -49,11 +49,29 @@ public class AllCodons {
         }
         return dna.substring(startIndex,minIndex + 3);
     }
+    
+    public StorageResource getAllGenes(String dna){
+        StorageResource geneList = new StorageResource();
+        
+        int startIndex = 0;
+        
+        while (true){
+            String currentGene = findGene(dna,startIndex);
+            if (currentGene.isEmpty()){
+                break;
+            }
+            geneList.add(currentGene);
+            startIndex = dna.indexOf(currentGene, startIndex) +
+                         currentGene.length();
+        }
+        return geneList;
+    }
+   
 
     public void testFindStop(){
         String dna = "xxxyyyzzzTAAxxxyyyzzzTAAxx";
         int dex = findStopCodon(dna,0,"TAA");
-        if(dex != -1) System.out.println("errorr on 9");
+        if(dex != -1) System.out.println("error on 9");
         
         dex = findStopCodon(dna,9,"TAA");
         if(dex != -1) System.out.println("errorr on 21");
@@ -62,17 +80,31 @@ public class AllCodons {
         if(dex != -1) System.out.println("errorr on 26");
         
         dex = findStopCodon(dna,0,"TAG");
-        if(dex != -1) System.out.println("errorr on 26 TAG");
+        if(dex != -1) System.out.println("error on 26 TAG");
         System.out.println("tests finished");
     }
     
     public void testFindGene(){
         String dna = "ATGCCCGGGAAATAACCC";
-        String gene = findGene(dna);
-        if (! gene.equals("ATGCCCGGGAAATAA")){
-            System.out.println("error");
+        //String gene = findGene(dna);
+        //if (! gene.equals("ATGCCCGGGAAATAA")){
+          //  System.out.println("error");
+        //}
+        //System.out.println("tests finished");
+    }
+    
+    public void testOn(String dna){
+        System.out.println("Testing printAllGenes on " + dna);
+        StorageResource genes = getAllGenes(dna);
+        for (String g:genes.data()){
+            System.out.println(g);
         }
-        System.out.println("tests finished");
+    }
+    
+    public void test(){
+        testOn("ATGATCTAATTTATGCTGCAACGGTGAAGA");
+        testOn("");
+        testOn("ATGATCATAAGAAGATAATAGAGGGCCATGTAA");
     }
     
 }
